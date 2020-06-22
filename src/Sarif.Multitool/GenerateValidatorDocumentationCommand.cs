9a6481d44f5d13b5cd22061cdf13a4effccac517
Larry Golding <larrygolding@comcast.net>
2020-06-22 09:40:48 -0700

    Introduce GenerateValidatorDocumentation{Command,Options}.

---
+++ src/Sarif.Multitool/GenerateValidatorDocumentationCommand.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.CodeAnalysis.Sarif.Driver;
+
+namespace Microsoft.CodeAnalysis.Sarif.Multitool
+{
+    internal class GenerateValidatorDocumentationCommand : CommandBase
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public GenerateValidatorDocumentationCommand(IFileSystem fileSystem = null)
+        {
+            _fileSystem = fileSystem ?? new FileSystem();
+        }
+
+        internal int Run(GenerateValidatorDocumentationOptions options)
+        {
+            try
+            {
+                if (!DriverUtilities.ReportWhetherOutputFileCanBeCreated(options.OutputFilePath, options.Force, _fileSystem))
+                {
+                    return FAILURE;
+                }
+
+                Console.WriteLine($"Writing validation rule documentation to '{options.OutputFilePath}'...");
+                _fileSystem.WriteAllText(options.OutputFilePath, "Hello, validation rule documentation!");
+                Console.WriteLine("Done.");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                return FAILURE;
+            }
+
+            return SUCCESS;
+        }
+    }
+}
---
+++ src/Sarif.Multitool/GenerateValidatorDocumentationOptions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using CommandLine;
+
+namespace Microsoft.CodeAnalysis.Sarif.Multitool
+{
+    /// <summary>
+    /// Options for the 'generate-validator-documentation' command, which creates a Markdown
+    /// document that describes the rules used by the 'validate' command.
+    /// </summary>
+    [Verb("generate-validator-documentation", HelpText = "Generate a Markdown document that describes the rules used by the 'validate' command.")]
+    internal class GenerateValidatorDocumentationOptions
+    {
+        [Option('o',
+            "output",
+            Required = false,
+            Default = "SarifValidationRules.md")]
+        public string OutputFilePath { get; set; }
+
+        [Option(
+            'f',
+            "force",
+            Default = false,
+            HelpText = "Force overwrite of output file if it exists.")]
+        public bool Force { get; set; }
+    }
+}
